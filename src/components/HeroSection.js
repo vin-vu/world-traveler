556b445ca358274b8ab49245849c582037303193
vin-vu <vince.hvu@gmail.com>
2021-08-12 23:34:30 -0700

    Updates to herosection, added herosection styling, created home component

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import Button from './Button.js';
+import '../App.css';
+import { Button } from './Button.js';
+import './HeroSection.css';
 
 function HeroSection() {
   return (
@@ -8,7 +10,20 @@ function HeroSection() {
       <h1>ADVENTURE AWAITS</h1>
       <p>What are you waiting for?
         <div className='hero-btns'>
-          <Button className='btns' buttonStyles='btn--outline' buttonSize='btn--large'>Get Started</Button>
+          <Button 
+            className='btns' 
+            buttonStyles='btn--outline' 
+            buttonSize='btn--large'
+          >
+            Get Started
+          </Button>
+          <Button 
+            className='btns'
+            buttonStyles='btn--primary'
+            buttonSize='btn--large'
+          >
+            Watch Trailer <i className='far fa-play-circle'/>
+          </Button>
         </div>
       </p>
     </div>
