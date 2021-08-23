e0b7673e3de97646613e00688711cc1c00a081fe
vin-vu <vince.hvu@gmail.com>
2021-08-22 21:47:21 -0700

    Fixed className typo bug

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function CardItem() {
+function CardItem(props) {
   return (
     <>
       <li className='cards__item'>
-        <Link clasasName='cards__item__link'>
-          <figure classNaame='cards__item__pic-wrap'>
-            <img src="/" alt="Travel image" className='cards__item_img'/>
+        <Link className='cards__item__link' to={props.path}>
+          <figure className='cards__item__pic-wrap' data-category={props.label}>
+            <img src={props.src} alt="Travel" className='cards__item__img'/>
           </figure>
-          <div classname='cards__item__info'>
-            <h5 className='cards__item__text'></h5>
+          <div className='cards__item__info'>
+            <h5 className='cards__item__text'>{props.text}</h5>
           </div>
         </Link>
       </li>
@@ -18,4 +18,4 @@ function CardItem() {
   )
 }
 
-export default CardItem
+export default CardItem;
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import CardItem from './CardItem';
+import './Cards.css';
 
 function Cards() {
   return (
@@ -8,10 +9,18 @@ function Cards() {
       <div className='cards__container'>
         <div className='cards__wrapper'>
           <ul className='cards_items'>
-            <CardItem />
+            <CardItem 
+            src='images/img-9.jpg'
+            text='Explore the hidden waterfall deep insidet the Amazon jungle'
+            label='Adventure'
+            path='/services'
+            />
           </ul>
         </div>
       </div>
     </div>
   )
 }
+
+export default Cards
+
