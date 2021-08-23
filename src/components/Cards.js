db581016568082ba75f7246c9e03ccd14a43c79c
vin-vu <vince.hvu@gmail.com>
2021-08-22 23:16:13 -0700

    Added 5 cards to home pages, created new page files

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -8,13 +8,39 @@ function Cards() {
       <h1>Check out these EPIC DESTINATIONS</h1>
       <div className='cards__container'>
         <div className='cards__wrapper'>
-          <ul className='cards_items'>
+          <ul className='cards__items'>
             <CardItem 
             src='images/img-9.jpg'
             text='Explore the hidden waterfall deep insidet the Amazon jungle'
             label='Adventure'
             path='/services'
             />
+            <CardItem 
+            src='images/img-2.jpg'
+            text='Travel through the islands of Bali'
+            label='Luxury'
+            path='/services'
+            />             
+          </ul>
+          <ul className='cards__items'>
+            <CardItem 
+            src='images/img-3.jpg'
+            text='Set sail in the wild waters of the Carribean'
+            label='Mystery'
+            path='/services'
+            />
+            <CardItem 
+            src='images/img-4.jpg'
+            text='Experience football in the Himalayan mountains'
+            label='Adventure'
+            path='/products'
+            />      
+            <CardItem 
+            src='images/img-8.jpg'
+            text='Ride camels to explore the pyramids in the Sahara'
+            label='Adrenaline'
+            path='/sign-up'
+            />          
           </ul>
         </div>
       </div>
