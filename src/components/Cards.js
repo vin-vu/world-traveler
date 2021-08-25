49d06af6dc127a3d77224cb9b540ce15e5896d03
vin-vu <vince.hvu@gmail.com>
2021-08-24 23:16:22 -0700

    Moved images to src to fix CRA bug

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -10,13 +10,14 @@ function Cards() {
         <div className='cards__wrapper'>
           <ul className='cards__items'>
             <CardItem 
-            src='images/img-9.jpg'
+            // src='../static/images/img-9.jpg'
+            src={require('../static/images/img-9.jpg').default}
             text='Explore the hidden waterfall deep insidet the Amazon jungle'
             label='Adventure'
             path='/services'
             />
             <CardItem 
-            src='images/img-2.jpg'
+            src={require('../static/images/img-2.jpg').default}
             text='Travel through the islands of Bali'
             label='Luxury'
             path='/services'
@@ -24,19 +25,19 @@ function Cards() {
           </ul>
           <ul className='cards__items'>
             <CardItem 
-            src='images/img-3.jpg'
+            src={require('../static/images/img-3.jpg').default}
             text='Set sail in the wild waters of the Carribean'
             label='Mystery'
             path='/services'
             />
             <CardItem 
-            src='images/img-4.jpg'
+            src={require('../static/images/img-4.jpg').default}
             text='Experience football in the Himalayan mountains'
             label='Adventure'
             path='/products'
             />      
             <CardItem 
-            src='images/img-8.jpg'
+            src={require('../static/images/img-8.jpg').default}
             text='Ride camels to explore the pyramids in the Sahara'
             label='Adrenaline'
             path='/sign-up'
