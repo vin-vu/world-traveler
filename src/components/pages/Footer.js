334edbe0411ce44d8083af4066a3abe0e7d267d5
vin-vu <vince.hvu@gmail.com>
2021-08-26 00:36:15 -0700

    Updates and added icons to footer

--- src/components/pages/Footer.js
+++ src/components/pages/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Button } from '../Button.js';
-import './Footer.css'
+import './Footer.css';
+import { Link } from 'react-router-dom';
 
 function Footer() {
   return (
@@ -24,14 +25,63 @@ function Footer() {
           <div className="footer-link-items">
             <h2>About Us</h2>
             <Link to='/sign-up'>How it works</Link>
-            <Link to='/sign-up'>Testimonials</Link>
-            <Link to='/sign-up'>Careers</Link>
-            <Link to='/sign-up'>Investors</Link>
-            <Link to='/sign-up'>Terms of Service</Link>
-
+            <Link to='/'>Testimonials</Link>
+            <Link to='/'>Careers</Link>
+            <Link to='/'>Investors</Link>
+            <Link to='/'>Terms of Service</Link>
+          </div>
+          <div className="footer-link-items">
+            <h2>Contact Us</h2>
+            <Link to='/'>Contacts</Link>
+            <Link to='/'>Support</Link>
+            <Link to='/'>Destinations</Link>
+            <Link to='/'>Sponsorships</Link>
+          </div>
+        </div>
+        <div className="footer-link-wrapper">
+          <div className="footer-link-items">
+            <h2>Videos</h2>
+            <Link to='/'>Submit Video</Link>
+            <Link to='/'>Ambaassadors</Link>
+            <Link to='/'>Agency</Link>
+            <Link to='/'>Influencer</Link>
+          </div>
+          <div className="footer-link-items">
+            <h2>Social Media</h2>
+            <Link to='/'>Instagram</Link>
+            <Link to='/'>Facebook</Link>
+            <Link to='/'>YouTube</Link>
+            <Link to='/'>Twitter</Link>
           </div>
         </div>
       </div>
+      <section className="social-media">
+        <div className="social-media-wrap">
+          <div className="footer-logo">
+            <Link className="social-logo">
+              TRVL <i className="fab fa-typo3" />
+            </Link>
+          </div>
+          <small className="website-rights">TRVL C 2020</small>
+          <div className="social-icons">
+            <Link className="social-icon-link facebook" to='/' target="_blank" aria-label="Facebook">
+              <i className="fab fa-facebook-f" />
+            </Link>
+            <Link className="social-icon-link instagram" to='/' target="_blank" aria-label="Instagram">
+              <i className="fab fa-instagram" />
+            </Link>
+            <Link className="social-icon-link youtube" to='/' target="_blank" aria-label="Youtube">
+              <i className="fab fa-youtube" />
+            </Link>
+            <Link className="social-icon-link twitter" to='/' target="_blank" aria-label="Twitter">
+              <i className="fab fa-twitter" />
+            </Link>
+            <Link className="social-icon-link linkedin" to='/' target="_blank" aria-label="LinkedIn">
+              <i className="fab fa-linkedin" />
+            </Link>
+          </div>
+        </div>
+      </section>
     </div>
   )
 }
