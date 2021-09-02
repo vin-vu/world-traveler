6dd4659fe398825052ce911742cd5a828f8a6c68
vin-vu <vince.hvu@gmail.com>
2021-09-02 00:14:53 -0700

    Updated cards.js to fix video background bug

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -2,51 +2,97 @@ import React from 'react';
 import CardItem from './CardItem';
 import './Cards.css';
 
+// function Cards() {
+//   return (
+//     <div classNamae='cards'>
+//       <h1>Check out these EPIC DESTINATIONS</h1>
+//       <div className='cards__container'>
+//         <div className='cards__wrapper'>
+//           <ul className='cards__items'>
+//             <CardItem 
+//             src={require('../static/images/img-9.jpg').default}
+//             text='Explore the hidden waterfall deep insidet the Amazon jungle'
+//             label='Adventure'
+//             path='/services'
+//             />
+//             <CardItem 
+//             src={require('../static/images/img-2.jpg').default}
+//             text='Travel through the islands of Bali'
+//             label='Luxury'
+//             path='/services'
+//             />             
+//           </ul>
+//           <ul className='cards__items'>
+//             <CardItem 
+//             src={require('../static/images/img-3.jpg').default}
+//             text='Set sail in the wild waters of the Carribean'
+//             label='Mystery'
+//             path='/services'
+//             />
+//             <CardItem 
+//             src={require('../static/images/img-4.jpg').default}
+//             text='Experience football in the Himalayan mountains'
+//             label='Adventure'
+//             path='/products'
+//             />      
+//             <CardItem 
+//             src={require('../static/images/img-8.jpg').default}
+//             text='Ride camels to explore the pyramids in the Sahara'
+//             label='Adrenaline'
+//             path='/sign-up'
+//             />          
+//           </ul>
+//         </div>
+//       </div>
+//     </div>
+//   )
+// }
+
 function Cards() {
   return (
-    <div classNamae='cards'>
-      <h1>Check out these EPIC DESTINATIONS</h1>
+    <div className='cards'>
+      <h1>Check out these EPIC Destinations!</h1>
       <div className='cards__container'>
         <div className='cards__wrapper'>
           <ul className='cards__items'>
-            <CardItem 
-            src={require('../static/images/img-9.jpg').default}
-            text='Explore the hidden waterfall deep insidet the Amazon jungle'
-            label='Adventure'
-            path='/services'
+            <CardItem
+              src={require('../static/images/img-9.jpg').default}
+              text='Explore the hidden waterfall deep inside the Amazon Jungle'
+              label='Adventure'
+              path='/services'
             />
-            <CardItem 
-            src={require('../static/images/img-2.jpg').default}
-            text='Travel through the islands of Bali'
-            label='Luxury'
-            path='/services'
-            />             
+            <CardItem
+              src={require('../static/images/img-2.jpg').default}
+              text='Travel through the Islands of Bali in a Private Cruise'
+              label='Luxury'
+              path='/services'
+            />
           </ul>
           <ul className='cards__items'>
-            <CardItem 
-            src={require('../static/images/img-3.jpg').default}
-            text='Set sail in the wild waters of the Carribean'
-            label='Mystery'
-            path='/services'
+            <CardItem
+              src={require('../static/images/img-3.jpg').default}
+              text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
+              label='Mystery'
+              path='/services'
             />
-            <CardItem 
-            src={require('../static/images/img-4.jpg').default}
-            text='Experience football in the Himalayan mountains'
-            label='Adventure'
-            path='/products'
-            />      
-            <CardItem 
-            src={require('../static/images/img-8.jpg').default}
-            text='Ride camels to explore the pyramids in the Sahara'
-            label='Adrenaline'
-            path='/sign-up'
-            />          
+            <CardItem
+              src={require('../static/images/img-4.jpg').default}
+              text='Experience Football on Top of the Himilayan Mountains'
+              label='Adventure'
+              path='/products'
+            />
+            <CardItem
+              src={require('../static/images/img-8.jpg').default}
+              text='Ride through the Sahara Desert on a guided camel tour'
+              label='Adrenaline'
+              path='/sign-up'
+            />
           </ul>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Cards
+export default Cards;
 
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -9,26 +9,26 @@ function HeroSection() {
     <div className='hero-container'>
       <video src={video} autoPlay loop muted />
       <h1>ADVENTURE AWAITS</h1>
-      <p>What are you waiting for?
-        <div className='hero-btns'>
-          <Button 
-            className='btns' 
-            buttonStyles='btn--outline' 
-            buttonSize='btn--large'
-          >
-            Get Started
-          </Button>
-          <Button 
-            className='btns'
-            buttonStyles='btn--primary'
-            buttonSize='btn--large'
-          >
-            Watch Trailer <i className='far fa-play-circle'/>
-          </Button>
-        </div>
-      </p>
+      <p>What are you waiting for?</p>
+      <div className='hero-btns'>
+        <Button
+          className='btns'
+          buttonStyle='btn--outline'
+          buttonSize='btn--large'
+        >
+          GET STARTED
+        </Button>
+        <Button
+          className='btns'
+          buttonStyle='btn--primary'
+          buttonSize='btn--large'
+          onClick={console.log('hey')}
+        >
+          WATCH TRAILER <i className='far fa-play-circle' />
+        </Button>
+      </div>
     </div>
-  )
+  );
 }
 
 export default HeroSection;
